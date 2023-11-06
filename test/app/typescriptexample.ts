214391dc9a5c14489e23d5f3f9fe8f403d82247f
Jeremy Tenjo <tenjojeremy@gmail.com>
2023-11-05 19:56:30 -0500

    Fix spelling of "grey" to "gray" in .prettierrc.js and typescriptexample.ts

--- test/app/typescriptexample.ts
+++ test/app/typescriptexample.ts
@@ -7,7 +7,7 @@ const colors: any = {
       primary: 'linear-gradient(83.6deg, #5D59FF 8.71%, #9076FF 98.37%)',
       red: 'linear-gradient(83.6deg, #FF5959 8.71%, #FF7676 98.37%)',
     },
-    grey: { main: '#8c8c8c', light: '#DEDEDE' },
+    gray: { main: '#8c8c8c', light: '#DEDEDE' },
     black: {
       main: '#1C1929',
       lighter: '#373543',
